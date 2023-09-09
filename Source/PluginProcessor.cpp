33f9db71b1fc5ab4550288574a0d5c3ebc29b07c
Sean Chambers <schambers80@gmail.com>
2023-09-09 18:00:24 -0500

    create update method for updating coefficients including DRYing up the updateCutFilters method

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -246,7 +246,7 @@ void SimpleEQAudioProcessor::updatePeakFilter(const ChainSettings &chainSettings
     updateCoefficients(rightChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
 }
 
-void SimpleEQAudioProcessor::updateCoefficients(Coefficients &old, Coefficients &replacements)
+void SimpleEQAudioProcessor::updateCoefficients(Coefficients &old, const Coefficients &replacements)
 {
     *old = *replacements;
 }
